e526a271cbdcaad4833adf50801ed32b02d2557e
Victor Herrera Ayos <pasiondelbarza@gmail.com>
2023-04-16 07:48:06 -0500

    nueva configuracion firebase: Base de datos

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,14 +1,17 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD9i7bogmKSkAu_eB0CqY2xLtUtBEZKGvY",
-    authDomain: "bookstore-ea5f3.firebaseapp.com",
-    databaseURL: "https://bookstore-ea5f3-default-rtdb.firebaseio.com",
-    projectId: "bookstore-ea5f3",
-    storageBucket: "bookstore-ea5f3.appspot.com",
-    messagingSenderId: "678045471115",
-    appId: "1:678045471115:web:8610d73f1a46e4d8971f8e"
-  };
-  
-  // Initialize Firebase
-initializeApp(firebaseConfig);
+  apiKey: "AIzaSyD9i7bogmKSkAu_eB0CqY2xLtUtBEZKGvY",
+  authDomain: "bookstore-ea5f3.firebaseapp.com",
+  databaseURL: "https://bookstore-ea5f3-default-rtdb.firebaseio.com",
+  projectId: "bookstore-ea5f3",
+  storageBucket: "bookstore-ea5f3.appspot.com",
+  messagingSenderId: "678045471115",
+  appId: "1:678045471115:web:8610d73f1a46e4d8971f8e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Base de datos
+export const db =  getFirestore(app)
