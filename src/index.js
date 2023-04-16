a4b1fcdb1e5b0f2f3613d0c2b3e28562ed747df7
Victor Herrera Ayos <pasiondelbarza@gmail.com>
2023-04-16 07:51:01 -0500

    Comente el ejemplo a conexion a  base de datos

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-import	'./Firebase/config';
-import { getFirestore,collection,getDocs} from 'firebase/firestore';
+//import { db } from './Firebase/config'; // base de datos
+//import {collection, getDocs } from 'firebase/firestore';
 
 ReactDOM.render(
 
@@ -13,10 +13,11 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-//obtener base de datos
-const db = getFirestore();
+/*
+// obtener base de datos ejemplo - https://firebase.google.com/docs/firestore/query-data/queries?hl=es-419
 const usuariosRef = collection(db, "usuarios");
-getDocs(usuariosRef).then(res=>console.log(res.docs.map(usuario =>({...usuario.data()}))))
+getDocs(usuariosRef).then(res => console.log(res.docs.map(usuario => ({ ...usuario.data() }))))
+*/
 
 
 
