b44eedfb522cec5cfceeeb5053b13d2e5b151f6a
Diego Benavides <diegojose2322@hotmail.com>
2023-04-15 21:11:11 -0500

    fix: ahora ña ruta raiz apunta hacia la landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom"
-import Login from './Componentes/Login';
+import Login from './Componentes/loginRegister/Login';
+import Landing from './Componentes/landingPage/landing';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={ <Login/> } />
+        <Route path="/" element={ <Landing/> } />
         <Route path="about" element={ <Login/> } />
         <Route path="contact" element={ <Login/> } />
       </Routes>
