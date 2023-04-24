a741f04e68f701deaa6165618fd45e6cccc7570b
Victor Herrera Ayos <pasiondelbarza@gmail.com>
2023-04-24 17:51:49 -0500

    register auth

--- src/Context/authContext.jsx
+++ src/Context/authContext.jsx
@@ -5,8 +5,8 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
-import { auth,db } from "./../Firebase/config";
-import { doc, getDoc } from 'firebase/firestore'
+import { auth, db } from "./../Firebase/config";
+import { doc, getDoc, setDoc, query, where, collection, getDocs } from 'firebase/firestore'
 
 
 const authContext = createContext();
@@ -17,27 +17,44 @@ export const useAuth = () => {
   return context;
 };
 
+// Application authentication provider
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-
-  const signup = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
+  // firebase's register
+  const signup = async (name, lastname, email, password, role) => {
+    const info_usuario = await createUserWithEmailAndPassword(auth, email, password).then((newUser) => {
+      return newUser
+    });
+    // search an user by his uid
+    const docRef = doc(db, `usuarios/${info_usuario.user.uid}`)
+    // Creating a firebase's document with the user's information
+    await setDoc(docRef, { name: name, lastname: lastname, email: email, password: password, role: parseInt(role) })
   };
 
+  // firebase's login
   const login = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
-  const role = async(usuario) =>{
+  // Query: User's role
+  const role = async (usuario) => {
     const docRef = doc(db, `usuarios/${usuario.uid}`)
-      const query = await getDoc(docRef)
-      const role = await query.data().role
-      return role
-      
+    const query = await getDoc(docRef)
+    const role = await query.data().role
+    return role
+
+  }
+  // Query: is That email exist?
+  const emailValidator = async (email) => {
+    const docRef = collection(db, `usuarios`)
+    const q = query(docRef, where("email", "==", `${email}`))
+    const isEmpty = (await getDocs(q)).empty;
+    return isEmpty
   }
 
+  // Firebase's sign Out
   const logout = () => {
     try {
       signOut(auth);
@@ -46,6 +63,7 @@ export function AuthProvider({ children }) {
     }
   }
 
+  // User session
   useEffect(() => {
     const unsubuscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
@@ -62,7 +80,8 @@ export function AuthProvider({ children }) {
         user,
         logout,
         loading,
-        role
+        role,
+        emailValidator
       }}
     >
       {children}
