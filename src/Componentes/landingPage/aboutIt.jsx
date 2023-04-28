85690de2d7f1afcad0d29bfd8542ef3ad6a13eb6
Diego Benavides <diegojose2322@hotmail.com>
2023-04-28 15:34:07 -0500

    feat: nuevo componente, aboutIt, este se usará para explicar que hace la app

---
+++ src/Componentes/landingPage/aboutIt.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class aboutIt extends Component {
+  render() {
+    return (
+      <>
+        <a name="aboutIt"></a>
+        <div className="container">
+          <div className="row">
+            <div className="card text-center  border-info  text-bg-dark ">
+              <div className="card-header">Featured</div>
+              <div className="card-body">
+                <p className="card-text lead mb-4 fw-semibold">
+                  {" "}
+                  ¿Necesitas una herramienta donde clasificar tus libros
+                  favoritos y no perderlos?.
+                  <br />
+                  Book Store está para tí; poseemos un apartado donde podras
+                  buscar tus obras favoritas, ver si sinopsis y posteriormente
+                  catalogarla como favorito, estos favoritos los podras ver
+                  desde tu perfil. Tambien poseemos un modulo de chat, para que
+                  puedas darnos feedback :).
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default aboutIt;
