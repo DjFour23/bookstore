aeae970e1c05ca465448dcb8ed7c795ac9357742
Diego Benavides <diegojose2322@hotmail.com>
2023-05-04 23:59:00 -0500

    feat: ahora los libros se cargan y el container esta hecho con una altura en especifico

--- src/Componentes/Users/HomeUser.jsx
+++ src/Componentes/Users/HomeUser.jsx
@@ -1,4 +1,6 @@
 import { useAuth } from "../../Context/authContext";
+import * as React from "react";
+import { getLibros } from "./../../Firebase/Api/api";
 
 // User view
 export function HomeUser() {
@@ -12,78 +14,117 @@ export function HomeUser() {
       console.error(error.message);
     }
   };
+  const [libros, setLibros] = React.useState([{}]);
+  // obtener libros
+  const getLinks = async () => {
+    const querySnapshot = await getLibros();
+    const docs = [];
+    querySnapshot.forEach((doc) => {
+      docs.push({ ...doc.data(), id: doc.id });
+    });
+    setLibros(docs);
+  };
+
+  // cargar los libros
+  React.useEffect(() => {
+    getLinks();
+  }, []);
+
+
+
+  const [searchResults, setSearchResults] = React.useState([]);
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleSearch = () => {
+    // Aquí implementa la lógica de búsqueda
+    const results = [
+      /* Resultados de búsqueda */
+    ];
+    setSearchResults(results);
+  };
 
   // HTML - User view
   return (
     <>
-      <div className="w-full max-w-xs m-auto text-black">
+      <div className="w-full max-w-xs m-auto text-black mb-5">
         <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <p className="text-xl mb-4">
-            welcome User: {user.displayName || user.email}
+            welcome {user.displayName || user.email}
           </p>
         </div>
       </div>
       <div className="container text-center">
         <div className="row">
-          <div className="col-10">
-            <div
-              className="container border album overflow-y-scroll"
-              style={{ height: "60vh" }}
-            >
-              <div class="card-group">
-                <div class="col-3">
-                  <div class="card shadow-sm mt-4 mh-100 mr-3 ">
-                    <img
-                      src="..."
-                      class="card-img-top rounded-circle"
-                      alt="..."
-                    />
-                    <div class="card-body">
-                      <h4 class="card-text">Tittle</h4>
-                      <div class="d-flex  ">
-                        {/* <div class="btn-group">
-                          <button
-                            type="button"
-                            class="btn btn-sm btn-outline-secondary"
-                          >
-                            View
-                          </button>
-                          <button
-                            type="button"
-                            class="btn btn-sm btn-outline-secondary"
-                          >
-                            Edit
-                          </button>
-                        </div> */}
-                        <small class="text-muted align-self-end">
-                          <i
-                            class="fa-sharp fa-solid fa-star fa-2xl"
-                            style={{ color: "#eeff00" }}
-                          ></i>
-                        </small>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+          <div className="col-sm-12 col-md-4 col-lg-2 mb-5">
+            <div class="card rounded-5">
+              <br></br>
+              <div class="card-body">
+                {/* {console.log(user)} */}
+                <h5 class="card-title">{user.displayName || user.email}</h5>
+                <br></br>
+                <a href="#" onClick={handleLogout} class="btn btn-danger">
+                  logout
+                </a>
               </div>
             </div>
           </div>
-
-          <div className="col-2">
-            <div className="container">
-              <div class="card rounded-5" style={{ width: 18 + "rem" }}>
-                <img src="..." class="card-img-top" alt="..." />
-                <div class="card-body">
-                  {console.log(user)}
-                  <h5 class="card-title">{user.displayName || user.email}</h5>
-                  <p class="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a href="#" onClick={handleLogout} class="btn btn-danger">
-                    logout
-                  </a>
-                </div>
+          {/* <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Buscar</button>
+      <ul>
+        {searchResults.map((result) => (
+          <li>{alert(result)}</li>
+        ))}
+      </ul> */}
+          <div className="col-sm-12 col-md-8 col-lg-10">
+            <div
+              className="
+                row 
+                g-4 
+                overflow-y-scroll 
+                border 
+                album 
+                container
+                h-25 
+                "
+            >
+              <div class="card-group">
+                {libros.map((item, id) => (
+                  <>
+                    <div className="col-lg-3 col-md-2 col-sm-1 col-xs-1 mb-2 p-2 ">
+                      <div className="card shadow-sm mt-4 p-1">
+                        <img class="card-img" src={item.caratula} alt="..." />
+                        <div class="card-img-overlay">
+                          <div className="card text-center  border-info  text-bg-dark ">
+                            <div className="card-body">
+                              <p className="card-text lead mb-4 fw-semibold">
+                              <h5 class="card-title">{item.nombre}</h5>
+                              </p>
+                            </div>
+                          </div>
+                          
+                          {/* <p class="card-text">
+                        {item.descripcion}
+                      </p> */}
+                          {/* <small class="text-muted align-self-end">
+                        <i
+                          class="fa-sharp fa-solid fa-star fa-2xl"
+                          style={{ color: "#eeff00" }}
+                        ></i>
+                      </small> */}
+                        </div>
+                        <div class="card-footer">
+                          <small>
+                            {item.disponible ? "Disponible" : "Rentado"}
+                          </small>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ))}
               </div>
             </div>
           </div>
