1162a97b28c4eff24388704e4f8076399427425e
Diego Benavides <diegojose2322@hotmail.com>
2023-05-06 16:57:01 -0500

    feat: ahora se pueden rentar y devolver libros, adicional a esto, los libros rentados se les cambia el color de la card

--- src/Componentes/Users/HomeUser.jsx
+++ src/Componentes/Users/HomeUser.jsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { useAuth } from "../../Context/authContext";
 import { getLibros } from "./../../Firebase/Api/api";
 import { useNavigate } from "react-router-dom";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../Firebase/config";
 //import { useChat } from "../Chat/useChat";
 //import { auth } from "./../../Firebase/config";
 //import swal from "sweetalert";
@@ -65,6 +67,45 @@ export function HomeUser() {
     }
   };
 
+  // const handleDisponibilidadChange = async (id, disponibilidad) => {
+  //   try {
+  //     const docRef = firebase.firestore().collection('libros').doc(id);
+  //     await docRef.update({ disponible: disponibilidad });
+  //     // Actualizar los libros mostrados en el componente
+  //     const updatedLibros = libros.map((libro) => {
+  //       if (libro.id === id) {
+  //         return { ...libro, disponible: disponibilidad };
+  //       } else {
+  //         return libro;
+  //       }
+  //     });
+  //     setLibros(updatedLibros);
+  //   } catch (error) {
+  //     console.error(error.message);
+  //   }
+  // };
+
+  const actualizarDisponibilidad = async (id, disponibilidad) => {
+    try {
+      const docRef = doc(db, "libros", id);
+      const updatedLibros = libros.map((libro) => {
+        if (libro.id === id) {
+          return { ...libro, disponible: disponibilidad };
+        } else {
+          return libro;
+        }
+      });
+
+      const updates = { disponible: disponibilidad };
+      // console.log(libros., nombre, disponibilidad);
+      setLibros(updatedLibros);
+      console.log("Disponibilidad actualizada correctamente.");
+      return updateDoc(docRef, updates);
+    } catch (error) {
+      console.error("Error al actualizar la disponibilidad:", error);
+    }
+  };
+
   // HTML - User view
   return (
     <>
@@ -109,7 +150,7 @@ export function HomeUser() {
                 album 
                 container
                 "
-                style={{minHeight: '49vh', maxHeight: '52vh'}}
+              style={{ minHeight: "49vh", maxHeight: "52vh" }}
             >
               <div className="card-group">
                 <div className="col-12 mb-4">
@@ -120,57 +161,78 @@ export function HomeUser() {
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                   />
-                  <button className=" col-2 border rounded-end-5" onClick={handleSearch}>
-                  <i class="fa-solid ">Buscar .</i>
-                  <i class="fa-solid fa-magnifying-glass fa-beat"></i></button>
+                  <button
+                    className=" col-2 border rounded-end-5"
+                    onClick={handleSearch}
+                  >
+                    <i class="fa-solid ">Buscar .</i>
+                    <i class="fa-solid fa-magnifying-glass fa-beat"></i>
+                  </button>
                 </div>
                 {libros.map((item, id) => (
                   <>
-                    {item.disponible ? (
-                      <div className="col-lg-3 col-md-2 col-sm-1 col-xs-1 mb-2 p-2 ">
-                        <div className="card shadow-sm mt-4 p-1">
-                          <img class="card-img" src={item.caratula} alt="..." style={{minHeight: '38vh', maxHeight: '40vh'}}/>
-                          <div class="card-img-overlay">
-                            <div className="card text-center  border-info  text-bg-dark ">
-                              <div className="card-body">
-                                <p className="card-text lead mb-4 fw-semibold">
-                                  <h5 class="card-title">{item.nombre}</h5>
-                                  <label class="fancy-checkbox">
-                                    <input
-                                      type="checkbox"
-                                      checked={
-                                        localStorage.getItem(
-                                          `${item.nombre}-${id}`
-                                        ) === "true"
-                                          ? true
-                                          : false
-                                      }
-                                      onChange={(e) => {
-                                        localStorage.setItem(
-                                          `${item.nombre}-${id}`,
-                                          e.target.checked
-                                        );
-                                        handleCheckboxChange(e, id);
-                                      }}
-                                    />
-                                    <i
-                                      class="fa-solid fa-star checked fa-lg"
-                                      style={{ color: "#eeff00" }}
-                                    ></i>
-                                    <i
-                                      class="fa-regular fa-star unchecked fa-lg"
-                                      style={{ color: "#eeff00" }}
-                                    ></i>
-                                  </label>
-                                </p>
-                              </div>
+                    <div className="col-lg-3 col-md-2 col-sm-1 col-xs-1 mb-2 p-2 ">
+                      <div className="card shadow-sm mt-4 p-1">
+                        <img
+                          class="card-img"
+                          src={item.caratula}
+                          alt="..."
+                          style={{ minHeight: "38vh", maxHeight: "40vh" }}
+                        />
+                        <div class="card-img-overlay">
+                          <div
+                            className={`card text-center border-info ${
+                              item.disponible
+                                ? "text-bg-dark"
+                                : "text-bg-danger"
+                            }`}
+                          >
+                            <div className="card-body">
+                              <p className="card-text lead mb-4 fw-semibold">
+                                <h5 class="card-title">{item.nombre}</h5>
+                                <label class="fancy-checkbox">
+                                  <input
+                                    type="checkbox"
+                                    checked={
+                                      localStorage.getItem(
+                                        `${item.nombre}-${id}`
+                                      ) === "true"
+                                        ? true
+                                        : false
+                                    }
+                                    onChange={(e) => {
+                                      localStorage.setItem(
+                                        `${item.nombre}-${id}`,
+                                        e.target.checked
+                                      );
+                                      handleCheckboxChange(e, id);
+                                    }}
+                                  />
+                                  <i
+                                    class="fa-solid fa-star checked fa-lg"
+                                    style={{ color: "#eeff00" }}
+                                  ></i>
+                                  <i
+                                    class="fa-regular fa-star unchecked fa-lg"
+                                    style={{ color: "#eeff00" }}
+                                  ></i>
+                                </label>
+                                <button
+                                  onClick={() =>
+                                    actualizarDisponibilidad(
+                                      item.id,
+                                      !item.disponible
+                                    )
+                                  }
+                                >
+                                  {item.disponible ? "Rentar" : "Devolver"}
+                                </button>
+                              </p>
                             </div>
                           </div>
                         </div>
                       </div>
-                    ) : (
-                      ""
-                    )}
+                    </div>
                   </>
                 ))}
               </div>
